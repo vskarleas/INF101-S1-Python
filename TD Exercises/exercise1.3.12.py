c9396ede55094b570245f3072a708f354a2ca873
vskarleas <vskarleas@icloud.com>
2021-09-24 20:09:12 +0200

    exercise 1.3.12 added

---
+++ TD Exercises/exercise1.3.12.py
@@ -0,0 +1,35 @@
+print("Nombres premiers")
+n = int(input("Add entier N positif:"))
+play = 2
+i = 2
+choise = "yes"
+while play > 1:
+    if n < 0:
+        print("This is not a postif N, try again")
+        n = int(input("Add entier N positif:"))
+    else:
+        while i < n and (n%i) != 0:
+            i = i + 1
+        if i == n:
+            omr = 200
+        else:
+            omr = 250
+        if omr == 200 :
+            print("The number", n, "is premier")
+            print("Do you want to play again ?")
+            choising = str(input("Reply with yes or no:"))
+            if choising == choise:
+                play = play + 1
+                n = int(input("Add entier N positif:"))
+            else:
+                play = 0
+        else:
+            print("This is not a premier number")
+            print("Do you want to play again ?")
+            choising = str(input("Reply with yes or no:"))
+            if choising == choise:
+                play = play + 1
+                n = int(input("Add entier N positif:"))
+            else:
+                play = 0
+print("The game is terminated")
