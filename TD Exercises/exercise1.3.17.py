253e5b00a69aac240e3caf97915d7ebd36165174
vskarleas <vskarleas@icloud.com>
2021-10-01 13:14:10 +0200

    updated exercise 17 with better variables

--- TD Exercises/exercise1.3.17.py
+++ TD Exercises/exercise1.3.17.py
@@ -1,52 +1,59 @@
 lines = int(input("Donner un entier L:"))
-print("Figure a")
-linesb = lines
-lining = 1
-lining3 = 1
+print("Figure A")
+keepLines = lines
+figureB = 1
+figureC = 1
+figureD = lines
+figureE = lines
+figureF = (lines+1) - lines
+spacesF = lines-1
+
 spacing = lines
-lining4 = lines
-lining5 = lines
-lining6 = (lines+1) - lines
-lining7 = lines-1
+
 for lines in range (1, lines+1):
     print("****")
 
-print("Figure b")
-for linesb in range (1, linesb+1):
-    while lining <= linesb:
-        klrc = ('*' * lining)
+print("----------")
+print("Figure B")
+for keepLines in range (1, keepLines+1):
+    while figureB <= keepLines:
+        klrc = ('*' * figureB)
         print(klrc)
         lines = lines - 1
-        lining = lining + 1
-print("Figure c")
-for linesb in range (1, linesb+1):
-    while lining3 <= linesb:
-        klrd = ('*' * lining3)
-        space = (' ' * spacing)
-        print(f"{space}{klrd}")
+        figureB = figureB + 1
+print("----------")
+print("Figure C")
+for keepLines in range (1, keepLines+1):
+    while figureC <= keepLines:
+        klrd = ('*' * figureC)
+        spaceC = (' ' * spacing)
+        print(f"{spaceC}{klrd}")
         lines = lines - 1
         spacing = spacing -1
-        lining3 = lining3 + 1
-print("Figure d")
-for linesb in range (1, linesb+1):
-    klrb = ('*' * lining5)
+        figureC = figureC + 1
+print("----------")
+print("Figure D")
+for keepLines in range (1, keepLines+1):
+    klrb = ('*' * figureD)
     print(klrb)
-    lining5 = lining5 - 1
-print("Figure e")
-for linesb in range (1, linesb+1):
-    klrb = ('*' * lining4)
-    space4 = (' ' * spacing)
-    print(f"{space4}{klrb}")
+    figureD = figureD - 1
+print("----------")
+print("Figure E")
+for keepLines in range (1, keepLines+1):
+    klrb = ('*' * figureE)
+    spaceE = (' ' * spacing)
+    print(f"{spaceE}{klrb}")
     spacing = spacing +1
-    lining4 = lining4 - 1
-print("Figure f")
-for linesb in range (1, linesb):
-    klrk = ('*' * lining6)
-    spacing6 = (' ' * lining7)
-    print(f"{spacing6}{klrk}{spacing6}")
+    figureE = figureE - 1
+print("----------")
+print("Figure F")
+for keepLines in range (1, keepLines):
+    klrk = ('*' * figureF)
+    spaceF = (' ' * spacesF)
+    print(f"{spaceF}{klrk}{spaceF}")
     spacing = spacing +1
-    lining7 = lining7 - 1
-    lining6 = lining6 + 2
-png = int(lining6/2)
+    spacesF = spacesF - 1
+    figureF = figureF + 2
+png = int(figureF/2)
 opn = ('*' * png)
 print(f"{opn}o{opn}")
