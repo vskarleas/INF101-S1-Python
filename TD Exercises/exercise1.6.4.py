6cacad88d9cc0f632092d2e3502a138e9ae15c86
vskarleas <vskarleas@icloud.com>
2021-10-18 21:01:17 +0200

    Update exercise1.6.4.py

--- TD Exercises/exercise1.6.4.py
+++ TD Exercises/exercise1.6.4.py
@@ -1,21 +1,19 @@
 def function(liste, x):
-    if x == liste[i][::1]:
+    if x == liste[::1]:
         appartient = True
     else:
         appartient = False
     return appartient
 
-def function(liste, x):
+def function3(liste, x):
     for i in range(len(liste)):
-        if liste[i]==x:
+        if liste[i] == x:
+            
             appartient = True
         else:
             appartient = False
+            
     return appartient
-
-liste = [5, 6, 7]
-x = 3
-print(function(liste, x))
 
 def function2(liste, x):
     if x in liste:
@@ -24,6 +22,10 @@ def function2(liste, x):
         appartient = False
     return appartient
 
+liste = [5, 6, 7]
+x = 5
+print(function(liste, x))
+print(function3(liste, x))
 print(function2(liste, x))
 
 #Exercise 5, 6, 7 are coming soon!!
